b6c18ddc779f8e654d2add28452c1350e2e360c0
briasari <briasari660@gmail.com>
2024-02-15 14:24:23 -0500

    Add project files.

---
+++ DifficultySettings/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace DifficultySettings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.easyButton = new System.Windows.Forms.Button();
+            this.heroNameLabel = new System.Windows.Forms.Label();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.beginButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // easyButton
+            // 
+            this.easyButton.BackColor = System.Drawing.Color.Black;
+            this.easyButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LimeGreen;
+            this.easyButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.easyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.easyButton.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyButton.ForeColor = System.Drawing.Color.White;
+            this.easyButton.Location = new System.Drawing.Point(88, 199);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(114, 49);
+            this.easyButton.TabIndex = 0;
+            this.easyButton.Text = "Easy";
+            this.easyButton.UseVisualStyleBackColor = false;
+            this.easyButton.Click += new System.EventHandler(this.easyButton_Click);
+            // 
+            // heroNameLabel
+            // 
+            this.heroNameLabel.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heroNameLabel.ForeColor = System.Drawing.Color.White;
+            this.heroNameLabel.Location = new System.Drawing.Point(33, 55);
+            this.heroNameLabel.Name = "heroNameLabel";
+            this.heroNameLabel.Size = new System.Drawing.Size(313, 31);
+            this.heroNameLabel.TabIndex = 1;
+            this.heroNameLabel.Text = "Enter your hero\'s name:";
+            // 
+            // userInput
+            // 
+            this.userInput.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInput.ForeColor = System.Drawing.Color.Black;
+            this.userInput.Location = new System.Drawing.Point(307, 51);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(209, 35);
+            this.userInput.TabIndex = 2;
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.BackColor = System.Drawing.Color.Black;
+            this.mediumButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Coral;
+            this.mediumButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Coral;
+            this.mediumButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mediumButton.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumButton.ForeColor = System.Drawing.Color.White;
+            this.mediumButton.Location = new System.Drawing.Point(227, 199);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(114, 49);
+            this.mediumButton.TabIndex = 3;
+            this.mediumButton.Text = "Medium";
+            this.mediumButton.UseVisualStyleBackColor = false;
+            this.mediumButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hardButton
+            // 
+            this.hardButton.BackColor = System.Drawing.Color.Black;
+            this.hardButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Firebrick;
+            this.hardButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Firebrick;
+            this.hardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hardButton.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardButton.ForeColor = System.Drawing.Color.White;
+            this.hardButton.Location = new System.Drawing.Point(367, 199);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(114, 49);
+            this.hardButton.TabIndex = 4;
+            this.hardButton.Text = "Hard";
+            this.hardButton.UseVisualStyleBackColor = false;
+            this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(129, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select your difficulty level:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // beginButton
+            // 
+            this.beginButton.BackColor = System.Drawing.Color.Black;
+            this.beginButton.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beginButton.ForeColor = System.Drawing.Color.White;
+            this.beginButton.Location = new System.Drawing.Point(134, 317);
+            this.beginButton.Name = "beginButton";
+            this.beginButton.Size = new System.Drawing.Size(289, 49);
+            this.beginButton.TabIndex = 6;
+            this.beginButton.Text = "Begin";
+            this.beginButton.UseVisualStyleBackColor = false;
+            this.beginButton.Click += new System.EventHandler(this.beginButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(12, 393);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(540, 86);
+            this.outputLabel.TabIndex = 7;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(564, 503);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.beginButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.heroNameLabel);
+            this.Controls.Add(this.easyButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Label heroNameLabel;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button beginButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ DifficultySettings/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DifficultySettings
+{
+    public partial class Form1 : Form
+    {
+        string username;
+        string difficulty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            difficulty = "medium";
+
+            easyButton.BackColor = Color.Black;
+            mediumButton.BackColor = Color.Coral;
+            hardButton.BackColor = Color.Black;
+        }
+
+        private void easyButton_Click(object sender, EventArgs e)
+        {
+            difficulty = "easy";
+
+            easyButton.BackColor = Color.LimeGreen;
+            mediumButton.BackColor = Color.Black;
+            hardButton.BackColor = Color.Black;
+        }
+
+        private void beginButton_Click(object sender, EventArgs e)
+        {
+            username = userInput.Text;
+            outputLabel.Text = $"Farewell, {username}!";
+            outputLabel.Text += $"\nI wish you the best of luck on the {difficulty} path!";
+
+        }
+
+        private void hardButton_Click(object sender, EventArgs e)
+        {
+            difficulty = "hard";
+
+            easyButton.BackColor = Color.Black;
+            mediumButton.BackColor = Color.Black;
+            hardButton.BackColor = Color.Firebrick;
+
+        }
+    }
+}
